ea95e8f5c5d3d0053cf269f0095d5e582702e3c8
eliseo <eliseoamayaguerrero@gmail.com>
2026-03-06 16:09:31 -0500

    #3 arreglo de los codigos

---
+++ js/Transferencia/punto_3.js
@@ -0,0 +1,8 @@
+let edad1 = Number(prompt("Ingrese la edad de la primera persona:"));
+let edad2 = Number(prompt("Ingrese la edad de la segunda persona:"));
+let edad3 = Number(prompt("Ingrese la edad de la tercera persona:"));
+let edad4 = Number(prompt("Ingrese la edad de la cuarta persona:"));
+
+let promedio = (edad1 + edad2 + edad3 + edad4) / 4;
+
+alert("El promedio de edad es este: " + promedio);
